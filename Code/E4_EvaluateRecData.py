026979ee2cb623600b0676f3fda08b6fe34d89af
Filips <fedotovs@hotmail.com>
2022-03-06 20:41:37 +0000

    Development

--- Code/E4_EvaluateRecData.py
+++ Code/E4_EvaluateRecData.py
@@ -73,7 +73,7 @@ test_data = pd.read_csv(test_file_location, header=0,
 test_data['Track_No']=test_data[args.Track]
 test_data_no=test_data.drop(['Hit_ID'],axis=1)
 test_data_no=test_data.groupby([args.Track],as_index=False).count()
-test_data_no = test_data_no[test_data.Track_No >= PM.MinHitsTrack]
+test_data_no = test_data_no[test_data_no.Track_No >= PM.MinHitsTrack]
 test_data=pd.merge(test_data, test_data_no, how="inner", on=[args.Track])
 print(test_data)
 exit()

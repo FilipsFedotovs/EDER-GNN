a570b81826ddbb002c57cdf2052568e9f106f117
Filips <fedotovs@hotmail.com>
2022-03-07 22:08:06 +0000

    Development

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -27,9 +27,9 @@ class HitCluster:
       def LoadClusterHits(self,RawHits): #Decorate hit information
            self.ClusterHits=[]
            for s in RawHits:
-               if s[1]>=ClusterID[0]*Step[0] and s[1]<((ClusterID[0]+1)*Step[0]):
-                   if s[2]>=ClusterID[1]*Step[1] and s[2]<((ClusterID[1]+1)*Step[1]):
-                       if s[3]>=ClusterID[2]*Step[2] and s[3]<((ClusterID[2]+1)*Step[2]):
+               if s[1]>=self.ClusterID[0]*self.Step[0] and s[1]<((self.ClusterID[0]+1)*self.Step[0]):
+                   if s[2]>=self.ClusterID[1]*self.Step[1] and s[2]<((self.ClusterID[1]+1)*self.Step[1]):
+                       if s[3]>=self.ClusterID[2]*self.Step[2] and s[3]<((self.ClusterID[2]+1)*self.Step[2]):
                           self.ClusterHits.append(s)
 class Track:
       def __init__(self,segments):

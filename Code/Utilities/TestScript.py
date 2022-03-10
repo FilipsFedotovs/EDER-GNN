67ae03cb42f02106252977b98cc1dcf8abca3cae
Filips <fedotovs@hotmail.com>
2022-03-10 21:26:11 +0000

    Development

--- Code/Utilities/TestScript.py
+++ Code/Utilities/TestScript.py
@@ -8,5 +8,5 @@ raw_data=[[1,1,2,1,1,1],[2,500,500,500,1,1],[3,999,999,800,1,1],[4,999,999,800,1
 hc.LoadClusterHits(raw_data)
 mc_data=[[1,1,2,1,1,1,'1'],[2,500,500,500,1,1,'2'],[3,999,999,800,1,1,'2'],[4,999,999,800,1,1,'1']]
 hc.LabelClusterHits(mc_data)
-data = train_test_split_edges(hc)
+data = train_test_split_edges(hc.ClusterGraph)
 print(data)

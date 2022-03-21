ea7b2634c71134324095b4f0fbba557351cd6ca7
Filips <fedotovs@hotmail.com>
2022-03-21 21:24:13 +0000

    Development

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -54,14 +54,14 @@ class HitCluster:
            _l_MCHits=pd.DataFrame(_MCClusterHits, columns = ['l_HitID','l_MC_ID'])
            _l_Hits=pd.DataFrame(self.ClusterHits, columns = ['l_HitID','l_x','l_y','l_z','l_tx','l_ty'])
            #Join hits + MC truth
-           _l_Tot_Hits=pd.merge(_l_MCHits, _l_Hits, how="right", on=['l_HitID'])
+           _l_Tot_Hits=pd.merge(_l_MCHits, _l_Hits, how="inner", on=['l_HitID'])
            _l_Tot_Hits['join_key'] = 'join_key'
 
            #Preparing Raw and MC combined data 2
            _r_MCHits=pd.DataFrame(_MCClusterHits, columns = ['r_HitID','r_MC_ID'])
            _r_Hits=pd.DataFrame(self.ClusterHits, columns = ['r_HitID','r_x','r_y','r_z','r_tx','r_ty'])
            #Join hits + MC truth
-           _r_Tot_Hits=pd.merge(_r_MCHits, _r_Hits, how="right", on=['r_HitID'])
+           _r_Tot_Hits=pd.merge(_r_MCHits, _r_Hits, how="inner", on=['r_HitID'])
            _r_Tot_Hits['join_key'] = 'join_key'
 
            #Combining data 1 and 2

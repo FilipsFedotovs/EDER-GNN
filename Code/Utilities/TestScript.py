f3e9e5be6f765a14e080a92bcc208d442144bb87
Filips <fedotovs@hotmail.com>
2022-03-29 11:01:42 +0100

    Development

--- Code/Utilities/TestScript.py
+++ Code/Utilities/TestScript.py
@@ -29,24 +29,25 @@ data=hc.ClusterGraph
 
 
 class Net(torch.nn.Module):
-     def __init__(self,sample):
-         super(Net, self).__init__()
-         self.conv1 = GCNConv(sample.num_features, 128)
-         self.conv2 = GCNConv(128, 64)
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = GCNConv(dataset.num_features, 128)
+        self.conv2 = GCNConv(128, 64)
 
-     def encode(self,sample):
-         x = self.conv1(sample.x, sample.train_pos_edge_index) # convolution 1
-         x = x.relu()
-         return self.conv2(x, sample.train_pos_edge_index) # convolution 2
+    def encode(self):
+        x = self.conv1(data.x, data.train_pos_edge_index) # convolution 1
+        x = x.relu()
+        return self.conv2(x, data.train_pos_edge_index) # convolution 2
 
-     def decode(self, z, pos_edge_index, neg_edge_index): # only pos and neg edges
-         edge_index = torch.cat([pos_edge_index, neg_edge_index], dim=-1) # concatenate pos and neg edges
-         logits = (z[edge_index[0]] * z[edge_index[1]]).sum(dim=-1)  # dot product
-         return logits
+    def decode(self, z, pos_edge_index, neg_edge_index): # only pos and neg edges
+        edge_index = torch.cat([pos_edge_index, neg_edge_index], dim=-1) # concatenate pos and neg edges
+        logits = (z[edge_index[0]] * z[edge_index[1]]).sum(dim=-1)  # dot product
+        return logits
 
-     def decode_all(self, z):
-         prob_adj = z @ z.t() # get adj NxN
-         return (prob_adj > 0).nonzero(as_tuple=False).t() # get predicted edge_list
+    def decode_all(self, z):
+        prob_adj = z @ z.t() # get adj NxN
+        return (prob_adj > 0).nonzero(as_tuple=False).t() # get predicted edge_list
+
 
 
 model, data = Net().to(device), data.to(device)
@@ -55,52 +56,53 @@ optimizer = torch.optim.Adam(params=model.parameters(), lr=0.01)
 #
 #
 def get_link_labels(pos_edge_index, neg_edge_index):
-     # returns a tensor:
-     # [1,1,1,1,...,0,0,0,0,0,..] with the number of ones is equel to the lenght of pos_edge_index
-     # and the number of zeros is equal to the length of neg_edge_index
-     E = pos_edge_index.size(1) + neg_edge_index.size(1)
-     link_labels = torch.zeros(E, dtype=torch.float, device=device)
-     link_labels[:pos_edge_index.size(1)] = 1.
-     return link_labels
-#
-#
-def train(sample):
-     model.train()
+    # returns a tensor:
+    # [1,1,1,1,...,0,0,0,0,0,..] with the number of ones is equel to the lenght of pos_edge_index
+    # and the number of zeros is equal to the length of neg_edge_index
+    E = pos_edge_index.size(1) + neg_edge_index.size(1)
+    link_labels = torch.zeros(E, dtype=torch.float, device=device)
+    link_labels[:pos_edge_index.size(1)] = 1.
+    return link_labels
 
-     neg_edge_index = negative_sampling(
-         edge_index=sample.train_pos_edge_index, #positive edges
-         num_nodes=sample.num_nodes, # number of nodes
-         num_neg_samples=sample.train_pos_edge_index.size(1)) # number of neg_sample equal to number of pos_edges
 #
-     optimizer.zero_grad()
 #
-     z = model.encode() #encode
-     link_logits = model.decode(z, sample.train_pos_edge_index, neg_edge_index) # decode
+def train():
+    model.train()
 
-     link_labels = get_link_labels(sample.train_pos_edge_index, neg_edge_index)
-     loss = F.binary_cross_entropy_with_logits(link_logits, link_labels)
-     loss.backward()
-     optimizer.step()
+    neg_edge_index = negative_sampling(
+        edge_index=data.train_pos_edge_index, #positive edges
+        num_nodes=data.num_nodes, # number of nodes
+        num_neg_samples=data.train_pos_edge_index.size(1)) # number of neg_sample equal to number of pos_edges
 
-     return loss
+    optimizer.zero_grad()
+
+    z = model.encode() #encode
+    link_logits = model.decode(z, data.train_pos_edge_index, neg_edge_index) # decode
+
+    link_labels = get_link_labels(data.train_pos_edge_index, neg_edge_index)
+    loss = F.binary_cross_entropy_with_logits(link_logits, link_labels)
+    loss.backward()
+    optimizer.step()
+
+    return loss
 #
 #
 @torch.no_grad()
-def test(sample):
-     model.eval()
-     perfs = []
-     for prefix in ["val", "test"]:
-         pos_edge_index = sample[f'{prefix}_pos_edge_index']
-         neg_edge_index = sample[f'{prefix}_neg_edge_index']
+def test():
+    model.eval()
+    perfs = []
+    for prefix in ["val", "test"]:
+        pos_edge_index = data[f'{prefix}_pos_edge_index']
+        neg_edge_index = data[f'{prefix}_neg_edge_index']
 
-         z = model.encode() # encode train
-         link_logits = model.decode(z, pos_edge_index, neg_edge_index) # decode test or val
-         link_probs = link_logits.sigmoid() # apply sigmoid
+        z = model.encode() # encode train
+        link_logits = model.decode(z, pos_edge_index, neg_edge_index) # decode test or val
+        link_probs = link_logits.sigmoid() # apply sigmoid
 
-         link_labels = get_link_labels(pos_edge_index, neg_edge_index) # get link
+        link_labels = get_link_labels(pos_edge_index, neg_edge_index) # get link
 
-         perfs.append(roc_auc_score(link_labels.cpu(), link_probs.cpu())) #compute roc_auc score
-     return perfs
+        perfs.append(roc_auc_score(link_labels.cpu(), link_probs.cpu())) #compute roc_auc score
+    return perfs
 
 best_val_perf = test_perf = 0
 for epoch in range(1, 2000):

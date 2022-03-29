6041821f1ab1485e4e1d69aef3bd19962346cdbc
Filips <fedotovs@hotmail.com>
2022-03-29 11:41:10 +0100

    Development

--- Code/Utilities/M2_TrainModel_Sub.py
+++ Code/Utilities/M2_TrainModel_Sub.py
@@ -251,6 +251,7 @@ for tc in range(0,len(TrainClusters)):
          test_perf = tmp_test_perf
          if epoch % 10 == 0:
             records.append([ClusterSet,tc, TrainClusters[tc].ClusterSize,epoch, train_loss.item(), best_val_perf, test_perf])
+            print(ClusterSet,tc, TrainClusters[tc].ClusterSize,epoch, train_loss.item(), best_val_perf, test_perf)
      #log = 'Cluster Set {:03d} Epoch: {:03d}, Loss: {:.4f}, Val: {:.4f}, Test: {:.4f}'
 
 # if ValidModel:

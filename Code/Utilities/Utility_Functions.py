40c3113fb12319ad2a9f39a5671d6cc645d23fe5
Filips <fedotovs@hotmail.com>
2022-04-24 17:58:05 +0100

    Development

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -240,7 +240,7 @@ class HitCluster:
               for h in range(len(self.HitLinks[0])):
                   print(ch[0],self.ClusterHitIDs[self.HitLinks[0][h]])
                   if ch[0]==self.ClusterHitIDs[self.HitLinks[0][h]]:
-                      _TempClusterHits.append(ch.append(self.ClusterHitIDs[self.HitLinks[1][h]]))
+                      _TempClusterHits.append(ch+self.ClusterHitIDs[self.HitLinks[1][h]])
                       print(_TempClusterHits)
                       exit()
 

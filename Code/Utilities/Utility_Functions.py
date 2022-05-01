eff7e4d4ff97cce258510ea76685edaa3fde3453
Filips <fedotovs@hotmail.com>
2022-05-01 16:06:23 +0100

    Development

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -364,8 +364,6 @@ class HitCluster:
                     if zz==len(z_ind)-1:
                         print(temp_s_hits)
                         temp_s_hits['Segment_ID']+=('-'+temp_s_hits['_l_HitID'])
-                        temp_s_hits['DoF']+=1
-                        temp_s_hits['Fit']+=temp_s_hits['link_strength']
                     temp_s_hits=temp_s_hits.drop(["_r_HitID",'r_z','link_strength'], axis=1)
                     temp_s_hits=temp_s_hits.rename(columns={"_l_HitID": "_r_HitID" })
 

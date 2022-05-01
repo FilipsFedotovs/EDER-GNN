b8a1f871f08103da24b68308095f9a20a24529b1
Filips <fedotovs@hotmail.com>
2022-05-01 15:01:41 +0100

    Development

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -351,7 +351,7 @@ class HitCluster:
 
                     temp_s_hits=pd.merge(temp_s_hits, temp_m_hits, how="right", on=['_r_HitID'])
 
-                    temp_s_hits['Segment_'+zz]=temp_s_hits['_r_HitID']
+                    temp_s_hits['Segment_'+str(zz)]=temp_s_hits['_r_HitID']
                     temp_s_hits['Segment_ID']+=('-'+temp_s_hits['_r_HitID'])
                     temp_s_hits['DoF']+=1
                     temp_s_hits['Fit']+=temp_s_hits['link_strength']

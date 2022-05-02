a6a1a4f9003999f1b1ba347cb4327b18b31fd2b4
Filips <fedotovs@hotmail.com>
2022-05-02 19:37:18 +0100

    Development

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -355,6 +355,7 @@ class HitCluster:
 
                         temp_s_hits['Segment_'+str(zz)]=temp_s_hits['_r_HitID']
                         temp_s_hits._l_HitID= temp_s_hits._l_HitID.fillna(temp_s_hits._r_HitID)
+                        temp_s_hits.link_strength= temp_s_hits.link_strength.fillna(temp_s_hits.link_strength)
                         print(temp_s_hits)
                         print(_Tot_Hits_Pool)
 

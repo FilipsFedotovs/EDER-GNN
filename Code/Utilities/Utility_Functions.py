7ca43dd5f2bf3dc8f42afe5939a38b3f4490925a
Filips <fedotovs@hotmail.com>
2022-05-13 18:32:44 +0100

    Development

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -558,6 +558,7 @@ class HitCluster:
           _Tot_Hits.drop(_Tot_Hits.index[_Tot_Hits['r_FEDRA_ID'] != _Tot_Hits['l_FEDRA_ID']], inplace = True)
           StatFakeValues.append(len(_Tot_Hits.axes[0])-len(_Tot_Hits.drop(_Tot_Hits.index[_Tot_Hits['l_MC_ID'] != _Tot_Hits['r_MC_ID']]).axes[0]))
           StatTruthValues.append(len(_Tot_Hits.drop(_Tot_Hits.index[_Tot_Hits['l_MC_ID'] != _Tot_Hits['r_MC_ID']]).axes[0]))
+          print(_Tot_Hits)
           print(StatFakeValues)
           print(StatTruthValues)
           exit()

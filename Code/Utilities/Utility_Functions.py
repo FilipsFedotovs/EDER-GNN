a77e42288ea16e8836ef3e24ab6dedeb8f61f5d0
Filips <fedotovs@hotmail.com>
2022-05-13 18:23:22 +0100

    Development

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -338,7 +338,7 @@ class HitCluster:
             _r_MCHits=pd.DataFrame(_MCClusterHits, columns = ['_r_HitID','l_MC_ID'])
             _l_Hits=_Tot_Hits_df.rename(columns={"x": "l_x", "y": "l_y", "z": "l_z", "tx": "l_tx","ty": "l_ty","_r_HitID": "_link_HitID" })
             #Join hits + MC truth
-            _l_Tot_Hits=pd.merge(_l_MCHits, _l_Hits, how="left", on=['_l_HitID'])
+            _l_Tot_Hits=pd.merge(_l_MCHits, _l_Hits, how="right", on=['_l_HitID'])
             #Preparing Raw and MC combined data 2
             _r_MCHits=pd.DataFrame(_MCClusterHits, columns = ['_r_HitID','r_MC_ID'])
             _r_Hits=_Tot_Hits_df[['_l_HitID', 'x', 'y', 'z', 'tx', 'ty']].rename(columns={"x": "r_x", "y": "r_y", "z": "r_z", "tx": "r_tx","ty": "r_ty","_l_HitID": "_r_HitID" })
@@ -532,8 +532,8 @@ class HitCluster:
           _l_Hits=_Tot_Hits_df.rename(columns={"z": "l_z","HitID": "l_HitID" })
           _r_Hits=_Tot_Hits_df.rename(columns={"z": "r_z","HitID": "r_HitID" })
           #Join hits + MC truth
-          _l_Tot_Hits=pd.merge(_l_MCHits, _l_Hits, how="left", on=['l_HitID'])
-          _r_Tot_Hits=pd.merge(_r_MCHits, _r_Hits, how="left", on=['r_HitID'])
+          _l_Tot_Hits=pd.merge(_l_MCHits, _l_Hits, how="right", on=['l_HitID'])
+          _r_Tot_Hits=pd.merge(_r_MCHits, _r_Hits, how="right", on=['r_HitID'])
           _l_Tot_Hits=pd.merge(_l_FHits, _l_Tot_Hits, how="right", on=['l_HitID'])
           _r_Tot_Hits=pd.merge(_r_FHits, _r_Tot_Hits, how="right", on=['r_HitID'])
           _l_Tot_Hits['join_key'] = 'join_key'

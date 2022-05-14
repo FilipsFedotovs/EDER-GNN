7c5dec5eece1064e231d2f4d5194ed8e43c87dde
Filips <fedotovs@hotmail.com>
2022-05-14 13:21:07 +0100

    Development

--- Code/Utilities/R3_LinkClusters_Sub.py
+++ Code/Utilities/R3_LinkClusters_Sub.py
@@ -66,75 +66,84 @@ print(UF.TimeStamp(),'Loading pre-selected data from ',input_file_location)
 data_file=open(input_file_location,'rb')
 RawClusters=pickle.load(data_file)
 data_file.close()
+if RawClusters[0].ClusterSize<2 and args.Log=='Y':
+    label=['Initial # of combinations','Delete self-permutations','Enforce positive directionality','Cut on delta t', 'Cut on delta x','Track Reconstruction']
+    array1=[0,0,0,0,0,0]
+    array2=[0,0,0,0,0,0]
+    RawClusters[0].RecStats=[label,array1,array2]
+    LoadedClusters=[]
+    LoadedClusters.append(RawClusters[0])
+    output_file_location=EOS_DIR+'/EDER-GNN/Data/REC_SET/R3_R4_LinkedClusters_'+str(Z_ID)+'_'+str(X_ID)+'_'+str(Y_ID)+'.pkl'
+    open_file = open(output_file_location, "wb")
+    pickle.dump(LoadedClusters, open_file)
+    print(UF.TimeStamp(), "Cluster linking is finished...")
+else:
+    print(UF.TimeStamp(),'Loading the model... ')
 
-print(UF.TimeStamp(),'Loading the model... ')
+    class Net(torch.nn.Module):
+                        def __init__(self,NoF):
+                            super(Net, self).__init__()
+                            self.conv1 = GCNConv(NoF, 128)
+                            self.conv2 = GCNConv(128, 64)
 
-class Net(torch.nn.Module):
-                    def __init__(self,NoF):
-                        super(Net, self).__init__()
-                        self.conv1 = GCNConv(NoF, 128)
-                        self.conv2 = GCNConv(128, 64)
+                        def encode(self,sample):
+                             x = self.conv1(sample.x, sample.train_pos_edge_index) # convolution 1
+                             x = x.relu()
+                             return self.conv2(x, sample.train_pos_edge_index) # convolution 2
 
-                    def encode(self,sample):
-                         x = self.conv1(sample.x, sample.train_pos_edge_index) # convolution 1
-                         x = x.relu()
-                         return self.conv2(x, sample.train_pos_edge_index) # convolution 2
+                        def decode(self, z, pos_edge_index, neg_edge_index): # only pos and neg edges
+                             edge_index = torch.cat([pos_edge_index, neg_edge_index], dim=-1) # concatenate pos and neg edges
+                             logits = (z[edge_index[0]] * z[edge_index[1]]).sum(dim=-1)  # dot product
+                             return logits
 
-                    def decode(self, z, pos_edge_index, neg_edge_index): # only pos and neg edges
-                         edge_index = torch.cat([pos_edge_index, neg_edge_index], dim=-1) # concatenate pos and neg edges
-                         logits = (z[edge_index[0]] * z[edge_index[1]]).sum(dim=-1)  # dot product
-                         return logits
+                        def decode_all(self, z):
+                             prob_adj = z @ z.t() # get adj NxN
+                             output_matrix=(prob_adj > 0).nonzero(as_tuple=False).t().tolist()
+                             strength_matrix=[]
+                             for i in range(len(output_matrix[0])):
+                                element=prob_adj[output_matrix[0][i]][output_matrix[1][i]].item()
+                                strength_matrix.append(element)
+                             output_matrix.append(strength_matrix)
+                             return output_matrix # get predicted edge_list
+    model = Net(5).to(device)
+    model.load_state_dict(torch.load(EOS_DIR+'/EDER-GNN/Models/'+args.ModelName))
+    model.eval()
+    if args.Log=='Y':
+        input_file_location=EOS_DIR+'/EDER-GNN/Data/TEST_SET/E1_HITS.csv'
+        MCdata=pd.read_csv(input_file_location,header=0,
+                                usecols=["Hit_ID","x","y","z","tx","ty",'MC_Mother_Track_ID'])
+        MCdata["x"] = pd.to_numeric(MCdata["x"],downcast='float')
+        MCdata["y"] = pd.to_numeric(MCdata["y"],downcast='float')
+        MCdata["z"] = pd.to_numeric(MCdata["z"],downcast='float')
+        MCdata["Hit_ID"] = MCdata["Hit_ID"].astype(str)
+        MCdata['z']=MCdata['z']-z_offset
+        MCdata['x']=MCdata['x']-x_offset
+        MCdata['y']=MCdata['y']-y_offset
+        MCdata.drop(MCdata.index[MCdata['z'] >= ((Z_ID+1)*stepZ)], inplace = True)  #Keeping the relevant z slice
+        MCdata.drop(MCdata.index[MCdata['z'] < (Z_ID*stepZ)], inplace = True)  #Keeping the relevant z slice
+        MCdata.drop(MCdata.index[MCdata['x'] >= ((X_ID+1)*stepX)], inplace = True)  #Keeping the relevant z slice
+        MCdata.drop(MCdata.index[MCdata['x'] < (X_ID*stepX)], inplace = True)  #Keeping the relevant z slice
+        MCdata.drop(MCdata.index[MCdata['y'] >= ((Y_ID+1)*stepY)], inplace = True)  #Keeping the relevant z slice
+        MCdata.drop(MCdata.index[MCdata['y'] < (Y_ID*stepY)], inplace = True)  #Keeping the relevant z slice
+        MCdata_list=MCdata.values.tolist()
 
-                    def decode_all(self, z):
-                         prob_adj = z @ z.t() # get adj NxN
-                         output_matrix=(prob_adj > 0).nonzero(as_tuple=False).t().tolist()
-                         strength_matrix=[]
-                         for i in range(len(output_matrix[0])):
-                            element=prob_adj[output_matrix[0][i]][output_matrix[1][i]].item()
-                            strength_matrix.append(element)
-                         output_matrix.append(strength_matrix)
-                         return output_matrix # get predicted edge_list
-model = Net(5).to(device)
-model.load_state_dict(torch.load(EOS_DIR+'/EDER-GNN/Models/'+args.ModelName))
-model.eval()
-if args.Log=='Y':
-    input_file_location=EOS_DIR+'/EDER-GNN/Data/TEST_SET/E1_HITS.csv'
-    MCdata=pd.read_csv(input_file_location,header=0,
-                            usecols=["Hit_ID","x","y","z","tx","ty",'MC_Mother_Track_ID'])
-    MCdata["x"] = pd.to_numeric(MCdata["x"],downcast='float')
-    MCdata["y"] = pd.to_numeric(MCdata["y"],downcast='float')
-    MCdata["z"] = pd.to_numeric(MCdata["z"],downcast='float')
-    MCdata["Hit_ID"] = MCdata["Hit_ID"].astype(str)
-    MCdata['z']=MCdata['z']-z_offset
-    MCdata['x']=MCdata['x']-x_offset
-    MCdata['y']=MCdata['y']-y_offset
-    MCdata.drop(MCdata.index[MCdata['z'] >= ((Z_ID+1)*stepZ)], inplace = True)  #Keeping the relevant z slice
-    MCdata.drop(MCdata.index[MCdata['z'] < (Z_ID*stepZ)], inplace = True)  #Keeping the relevant z slice
-    MCdata.drop(MCdata.index[MCdata['x'] >= ((X_ID+1)*stepX)], inplace = True)  #Keeping the relevant z slice
-    MCdata.drop(MCdata.index[MCdata['x'] < (X_ID*stepX)], inplace = True)  #Keeping the relevant z slice
-    MCdata.drop(MCdata.index[MCdata['y'] >= ((Y_ID+1)*stepY)], inplace = True)  #Keeping the relevant z slice
-    MCdata.drop(MCdata.index[MCdata['y'] < (Y_ID*stepY)], inplace = True)  #Keeping the relevant z slice
-    MCdata_list=MCdata.values.tolist()
-
-LoadedClusters=[]
-data=RawClusters[0].ClusterGraph
-top=[]
-bottom=[]
-for i in range(RawClusters[0].ClusterSize):
-            top.append(i)
-            bottom.append(i)
-data.train_pos_edge_index=torch.tensor(np.array([top,bottom]))
-print(RawClusters[0].ClusterSize)
-print(MCdata_list)
-lat_z = model.encode(data)
-if args.Log=='Y':
-            RawClusters[0].LinkHits(model.decode_all(lat_z),True,MCdata_list,cut_dt,cut_dr)
-LoadedClusters.append(RawClusters[0])
-output_file_location=EOS_DIR+'/EDER-GNN/Data/REC_SET/R3_R4_LinkedClusters_'+str(Z_ID)+'_'+str(X_ID)+'_'+str(Y_ID)+'.pkl'
-open_file = open(output_file_location, "wb")
-pickle.dump(LoadedClusters, open_file)
-print(UF.TimeStamp(), "Cluster linking is finished...")
-#End of the script
+    LoadedClusters=[]
+    data=RawClusters[0].ClusterGraph
+    top=[]
+    bottom=[]
+    for i in range(RawClusters[0].ClusterSize):
+                top.append(i)
+                bottom.append(i)
+    data.train_pos_edge_index=torch.tensor(np.array([top,bottom]))
+    lat_z = model.encode(data)
+    if args.Log=='Y':
+                RawClusters[0].LinkHits(model.decode_all(lat_z),True,MCdata_list,cut_dt,cut_dr)
+    LoadedClusters.append(RawClusters[0])
+    output_file_location=EOS_DIR+'/EDER-GNN/Data/REC_SET/R3_R4_LinkedClusters_'+str(Z_ID)+'_'+str(X_ID)+'_'+str(Y_ID)+'.pkl'
+    open_file = open(output_file_location, "wb")
+    pickle.dump(LoadedClusters, open_file)
+    print(UF.TimeStamp(), "Cluster linking is finished...")
+    #End of the script
 
 
 

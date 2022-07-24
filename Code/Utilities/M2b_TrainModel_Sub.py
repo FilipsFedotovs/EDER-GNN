bddd26e5903fef686e923f4d01ddab9631df0069
Filips <fedotovs@hotmail.com>
2022-07-24 19:39:00 +0100

    Experiment with Interactrion Network, change data input format

--- Code/Utilities/M2b_TrainModel_Sub.py
+++ Code/Utilities/M2b_TrainModel_Sub.py
@@ -89,7 +89,7 @@ print(UF.TimeStamp(), bcolors.OKGREEN+"Modules Have been imported successfully..
 def train(args, model, device, sample, optimizer, epoch):
     model.train()
     losses, t0, N = [], time(), len(sample)
-    for batch_idx, (data, fname) in enumerate(sample):
+    for HC in enumerate(sample):
         data = data.to(device)
         if (len(data.x)==0): continue
         optimizer.zero_grad()

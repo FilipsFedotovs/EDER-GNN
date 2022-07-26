c07909b869ae701403696a77da4a2ceae6eb176c
Filips <fedotovs@hotmail.com>
2022-07-26 21:56:23 +0100

    Experiment with Interactrion Network, change data input format

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -163,9 +163,9 @@ class HitCluster:
            import torch
            self.ClusterGraph.edge_index=torch.tensor([(HitCluster.GenerateLinks(_FinalList,self.ClusterHitIDs))])
            self.ClusterGraph.edge_attr=torch.tensor([(HitCluster.GenerateEdgeAttributes(_FinalList))])
-           print(self.ClusterGraph.x)
-           print(self.ClusterGraph.edge_index)
-           print(self.ClusterGraph.edge_attr)
+           print(self.ClusterGraph.x.dtype)
+           print(self.ClusterGraph.edge_index.dtype)
+           print(self.ClusterGraph.edge_attr.dtype)
       def GiveStats(self,MCHits,cut_dt, cut_dr): #Decorate hit information
            import pandas as pd
            _MCClusterHits=[]

9194c908d2e145fedb7b8f9672607fc5c0ff3238
Filips <fedotovs@hotmail.com>
2022-07-28 21:58:30 +0100

    Experiment with Interactrion Network, change data input format

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -148,6 +148,7 @@ class HitCluster:
            _Tot_Hits['d_y'] = _Tot_Hits['d_y'].abs()
            _Tot_Hits.drop(_Tot_Hits.index[_Tot_Hits['d_x'] >= cut_dr], inplace = True)
            _Tot_Hits.drop(_Tot_Hits.index[_Tot_Hits['d_y'] >= cut_dr], inplace = True)
+           _Tot_Hits = _Tot_Hits.drop(['join_key','r_x','r_y','r_z','l_x','l_y','l_z','l_MC_ID','r_MC_ID'],axis=1)
            print(_Tot_Hits)
            exit()
            _Genuine=_Tot_Hits.drop(_Tot_Hits.index[_Tot_Hits['l_MC_ID'] != _Tot_Hits['r_MC_ID']])

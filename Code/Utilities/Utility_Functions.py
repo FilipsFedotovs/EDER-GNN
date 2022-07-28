fee289bfab624118760e2c082184df5b5ed817c5
Filips <fedotovs@hotmail.com>
2022-07-28 22:48:22 +0100

    Experiment with Interactrion Network, change data input format

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -150,7 +150,7 @@ class HitCluster:
            _Tot_Hits.drop(_Tot_Hits.index[_Tot_Hits['d_y'] >= cut_dr], inplace = True)
            _Tot_Hits = _Tot_Hits.drop(['d_tx','d_ty','d_x','d_y','join_key','l_tx','l_ty','r_tx','r_ty'],axis=1)
            _Tot_Hits['label']=(_Tot_Hits['l_MC_ID']==_Tot_Hits['r_MC_ID']).astype('int8')
-           _Tot_Hits['d_l'] = math.sqrt((_Tot_Hits['r_y']-_Tot_Hits['l_y'])**2 + (_Tot_Hits['r_x']-_Tot_Hits['l_x'])**2 + (_Tot_Hits['r_z']-_Tot_Hits['l_z'])**2)
+           _Tot_Hits['d_l'] = (((_Tot_Hits['r_y']-_Tot_Hits['l_y'])**2) + ((_Tot_Hits['r_x']-_Tot_Hits['l_x'])**2) + ((_Tot_Hits['r_z']-_Tot_Hits['l_z'])**2))
            print(_Tot_Hits)
            exit()
            _Genuine=_Tot_Hits.drop(_Tot_Hits.index[_Tot_Hits['l_MC_ID'] != _Tot_Hits['r_MC_ID']])

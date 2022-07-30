89d52b7d46defce846b8cfea2016c565b65fa649
Filips <fedotovs@hotmail.com>
2022-07-30 22:46:49 +0100

    Experiment with Interactrion Network, change data input format

--- Code/Utilities/M2b_TrainModel_Sub.py
+++ Code/Utilities/M2b_TrainModel_Sub.py
@@ -164,10 +164,11 @@ def test(model, device, sample, thld):
             output = model(data.x, data.edge_index, data.edge_attr)
             y, output = data.y.float(), output.squeeze()
             acc, TPR, TNR = binary_classification_stats(output, y, thld)
-            print(acc.item())
-            print(output,y)
-            loss = F.binary_cross_entropy(output, y,
-                                          reduction='mean')
+            try:
+                loss = F.binary_cross_entropy(output, y,reduction='mean')
+            except:
+                print(y, output)
+                exit()
             print(acc.item())
             print(loss.item())
             accs.append(acc.item())
